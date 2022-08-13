5aa9a9262cfb03db43d2e4e4119ec3974743a5df
pvshmyglev <pshm2@mail.ru>
2022-08-14 01:08:40 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/ConvertEntity.kt
@@ -0,0 +1,29 @@
+package ru.netology.nmedia
+
+internal fun PostEntity.toPost() = Post (
+
+    id = id,
+    author = author,
+    content = content,
+    video = video,
+    publishedDate = publishedDate,
+    likeByMe = likeByMe,
+    countLikes = countLikes,
+    countShare = countShare,
+    countVisibility = countVisibility
+
+)
+
+internal fun Post.toPostEntity() = PostEntity (
+
+    id = id,
+    author = author,
+    content = content,
+    video = video,
+    publishedDate = publishedDate,
+    likeByMe = likeByMe,
+    countLikes = countLikes,
+    countShare = countShare,
+    countVisibility = countVisibility
+
+)
